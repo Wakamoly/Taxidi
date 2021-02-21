8ce960f64b2e3f7a11fdf53c8b28354283743d8a
Brendan Abbott <lucidsoftworksllc@gmail.com>
2021-02-20 21:23:04 -0500

    Adding test files

---
+++ app/src/androidTest/java/testUtils/DataGenerator.kt
@@ -0,0 +1,64 @@
+package testUtils
+
+object DataGenerator {
+
+    /**
+     * Database model creators/getters
+     */
+    /*fun reminderDTO1(): ReminderDTO {
+        return ReminderDTO(
+            title = "Google",
+            description = "Plex",
+            location = "Googleplex",
+            latitude = 37.422004,
+            longitude = -122.086246
+        )
+    }
+
+    fun reminderDTO2(): ReminderDTO {
+        return ReminderDTO(
+            title = "Uda",
+            description = "City",
+            location = "Udacity",
+            latitude = 37.399437,
+            longitude = -122.108060
+        )
+    }
+
+    *//**
+     * Domain model creators/getters
+     *//*
+    fun reminderValidDI(): ReminderDataItem {
+        return ReminderDataItem(
+            title = "Title",
+            description = "Description",
+            location = "Googleplex",
+            latitude = 37.422004,
+            longitude = -122.086246
+        )
+    }
+
+    *//**
+     * Testing domain models with null values
+     *//*
+    fun reminderNullTitleDI(): ReminderDataItem {
+        return ReminderDataItem(
+            title = null,
+            description = "Description",
+            location = "Googleplex",
+            latitude = 37.422004,
+            longitude = -122.086246
+        )
+    }
+
+    fun reminderNullLocationDI(): ReminderDataItem {
+        return ReminderDataItem(
+            title = "Title",
+            description = "Description",
+            location = null,
+            latitude = null,
+            longitude = null
+        )
+    }*/
+
+}
---
+++ app/src/androidTest/java/testUtils/Mappers.kt
@@ -0,0 +1,16 @@
+package testUtils
+
+object Mappers{
+
+    /*fun ReminderDTO.toDomainModel(): ReminderDataItem {
+        return ReminderDataItem(
+                this.title,
+                this.description,
+                this.location,
+                this.latitude,
+                this.longitude,
+                this.id
+        )
+    }*/
+
+}
